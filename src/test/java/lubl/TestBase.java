e76a5ff931180b95a97f606e51bf49390d20461e
Lyubov Rodomanova <lrodomanova81@gmail.com>
2022-05-09 16:53:42 +0300

    attach

--- src/test/java/lubl/TestBase.java
+++ src/test/java/lubl/TestBase.java
@@ -30,4 +30,12 @@ public class TestBase {
         capabilities.setCapability("enableVideo", true);
         Configuration.browserCapabilities = capabilities;
     }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+    }
 }
